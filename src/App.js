1b63063d043d57e2268b8b556ddde4bc63fb62e5
shaima <shaima.shalbi@esprit.tn>
2020-08-05 13:40:10 +0200

    3eme commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import  HomePage  from './pages/homepage/homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component.jsx';
+const HatsPage = () => (
+  <div>
+    <h1> Hats Page </h1>
+  </div>
+);
 
 function App() {
   return (
     <div >
-      <HomePage></HomePage>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
+
     </div>
   );
 }
